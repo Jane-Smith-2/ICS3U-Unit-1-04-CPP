ec8507c9ebf809a5c04bd844bf70d25a082b2afa
Jane Smith <jame.smith@mths.ca>
2020-07-05 12:42:45 +0000

    init

---
+++ basic_math.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by Mr. Coxall
+// Created on June 2020
+// This program does basic math
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "9+2=" << (9+2) << std::endl;
+    std::cout << "7-2=" << (7-2) << std::endl;
+    std::cout << "4/2=" << (4/2) << std::endl;
+    std::cout << "4+4/2=" << (4+4/2) << std::endl;
+    std::cout << "3+4*2=" << (3+4*2) << std::endl;
+    std::cout << "5+2^3=" << (5+2^3) << std::endl;
+    std::cout << "5+pow(2, 3)=" << (5+pow(2, 3)) << std::endl;
+}
